363bb5319301e19f250b41eaee6d36c37f302ccb
bellaanggita <bellaatari@gmail.com>
2020-10-04 22:14:55 +0700

    menambahkan page login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
 });
+
+Route::get('/login', 'AdminController@login');
+Route::get('/dashboard', 'AdminController@dashboard');
+
